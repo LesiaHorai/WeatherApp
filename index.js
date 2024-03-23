dfe9cb27be19e2b7e5b58a3f683c3633fd199aeb
LesiaHorai <lesyagoray@gmail.com>
2024-03-23 10:37:35 +0100

    Inject the forecast HTML from JavaScript

--- index.js
+++ index.js
@@ -70,3 +70,31 @@ function displayWeather(response) {
     body.style.backgroundImage = "url('images/background.jpg')";
   }
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast">
+          <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">
+                15º</span>
+              <span class="weather-forecast-temperature-min">
+                8º</span>      
+            </div> 
+            </div> 
+            </div>
+          </div>
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
